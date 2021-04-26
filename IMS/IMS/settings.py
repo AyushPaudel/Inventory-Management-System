b85a309adf8cefb1c601732d5b7ac86439a1d731
AyushPaudel <ap40021319@student.ku.edu.np>
2021-04-26 12:59:40 +0545

    small model

--- IMS/IMS/settings.py
+++ IMS/IMS/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     #my apps:
     'users.apps.UsersConfig',
-    'products.apps.ProductConfig'
+    'products.apps.ProductsConfig'
 ]
 
 MIDDLEWARE = [
