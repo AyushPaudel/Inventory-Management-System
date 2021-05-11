8e858a4670c0d6d5d1a8567dd7d3ad9454f83582
AnuragTimilsina <anurag.timilsina@gmail.com>
2021-05-11 19:45:42 +0545

    App name conflict resolved

--- IMS/IMS/settings.py
+++ IMS/IMS/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-AUTH_USER_MODEL = 'users.imsUser'
+AUTH_USER_MODEL = 'ims_users.imsUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
