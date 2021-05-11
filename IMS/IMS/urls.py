4b1ec0765d62b8e195e37aa8d1073ee149727c45
AnuragTimilsina <anurag.timilsina@gmail.com>
2021-05-11 20:04:37 +0545

    migration issues fixed

--- IMS/IMS/urls.py
+++ IMS/IMS/urls.py
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include('users.urls')),
+    path('', include('ims_users.urls')),
     path('', schema_view.with_ui('swagger',
         cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
